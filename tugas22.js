fcd7a222fa122bf5dc6b63df1d805ae5dea7fa0c
Wahyu Krishna <hanzo180895@gmail.com>
2021-03-07 22:58:54 +0700

    Tugas 22 JS
    Tugas Niomic

---
+++ tugas22.js
@@ -0,0 +1,9 @@
+const tugas22 = () =>{
+    var data = 'Saya ingin belajar bersama';
+    var result = data.split(' ');
+    result.forEach(function (item,index){
+        console.log('Item :',item,'Index ke',index);
+    })
+}
+
+tugas22();
